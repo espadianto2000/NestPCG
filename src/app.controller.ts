bf928afb355ced95cc6bf34877e0d59ae3c82eb8
espadianto2000 <20170532@aloe.ulima.edu.pe>
2022-09-12 01:59:43 -0500

    pruebaTemp

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,6 +9,8 @@ import { NivelCompletadoAdapt } from './models/nivelCompletadoAdapt';
 import { muerteUsuarioStatic } from './models/muerteUsuarioStatic';
 import { salaCompletadaStatic } from './models/salaCompletadaStatic';
 import { nivelCompletadoStatic } from './models/nivelCompletadoStatic';
+import { Cellular } from './models/cellular.model';
+import { Tilemap } from './models/tilemap.model';
 
 @Controller()
 export class AppController {
@@ -115,5 +117,68 @@ export class AppController {
     var nivelF :nivelCompletadoStatic = body;
     set(ref(database, 'users/'+nivelF.usuario+'/'+nivelF.inicioRun+'/'+nivelF.nivel+'/NivelFinalizado'), nivelF);
   }
-
+  @Post('/cellularAdapt')
+  cellularAdapt(@Body() body:Cellular):void{
+    const firebaseConfig = {
+      apiKey: "AIzaSyC6xePrFT1bd7fxGspNLSgzFTO44-hYy48",
+      authDomain: "pcgnest.firebaseapp.com",
+      databaseURL: "https://pcgnest-default-rtdb.firebaseio.com",
+      projectId: "pcgnest",
+      storageBucket: "pcgnest.appspot.com",
+      messagingSenderId: "367952707578",
+      appId: "1:367952707578:web:f6a2fdf4e35c9240dbae43"
+    };
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+    var cellularA:Cellular = body;
+    set(ref(database, 'algorithms/Cellular/Adatpt/'+Date.now()),cellularA);
+  }
+  @Post('/cellularStatic')
+  cellularStatic(@Body() body:Cellular):void{
+    const firebaseConfig = {
+      apiKey: "AIzaSyC6xePrFT1bd7fxGspNLSgzFTO44-hYy48",
+      authDomain: "pcgnest.firebaseapp.com",
+      databaseURL: "https://pcgnest-default-rtdb.firebaseio.com",
+      projectId: "pcgnest",
+      storageBucket: "pcgnest.appspot.com",
+      messagingSenderId: "367952707578",
+      appId: "1:367952707578:web:f6a2fdf4e35c9240dbae43"
+    };
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+    var cellularA:Cellular = body;
+    set(ref(database, 'algorithms/Cellular/Static/'+Date.now()),cellularA);
+  }
+  @Post('/tileMapAdapt')
+  tileMapAdapt(@Body() body:Tilemap):void{
+    const firebaseConfig = {
+      apiKey: "AIzaSyC6xePrFT1bd7fxGspNLSgzFTO44-hYy48",
+      authDomain: "pcgnest.firebaseapp.com",
+      databaseURL: "https://pcgnest-default-rtdb.firebaseio.com",
+      projectId: "pcgnest",
+      storageBucket: "pcgnest.appspot.com",
+      messagingSenderId: "367952707578",
+      appId: "1:367952707578:web:f6a2fdf4e35c9240dbae43"
+    };
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+    var tilemap:Tilemap = body;
+    set(ref(database, 'algorithms/Tilemap/Adapt/'+Date.now()),tilemap);
+  }
+  @Post('/tileMapStatic')
+  tileMapStatic(@Body() body:Tilemap):void{
+    const firebaseConfig = {
+      apiKey: "AIzaSyC6xePrFT1bd7fxGspNLSgzFTO44-hYy48",
+      authDomain: "pcgnest.firebaseapp.com",
+      databaseURL: "https://pcgnest-default-rtdb.firebaseio.com",
+      projectId: "pcgnest",
+      storageBucket: "pcgnest.appspot.com",
+      messagingSenderId: "367952707578",
+      appId: "1:367952707578:web:f6a2fdf4e35c9240dbae43"
+    };
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+    var tilemap:Tilemap = body;
+    set(ref(database, 'algorithms/Tilemap/Static/'+Date.now()),tilemap);
+  }
 }
